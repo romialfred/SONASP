60612c12b06ce1b3161538780bd6c9e776bad0ea
DATA UNIVERSE <46369236+romialfred@users.noreply.github.com>
2025-12-26 19:16:04 +0000

    Deleted FREIGHT_CUSTOMS_READY.md

--- ANALYZE_SALE_STATUS_ENUM.sql
+++
@@ -1,7 +0,0 @@
--- Analyser l'enum sale_status et trouver TOUTES les valeurs valides
-SELECT 
-  enumlabel as valid_status_value,
-  enumsortorder as sort_order
-FROM pg_enum
-WHERE enumtypid = (SELECT oid FROM pg_type WHERE typname = 'sale_status')
-ORDER BY enumsortorder;
